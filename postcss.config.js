41d6e5287d696ba6236011e2cfdbe3faaea99139
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-08 17:45:47 -0400

    Setup postcss plugins

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+  plugins: [
+    postcssPresetEnv({
+      stage: false,
+      features: {
+        'custom-media-queries': true,
+        'media-query-ranges': true,
+        'custom-selectors': true,
+      },
+    }),
+    require('cssnano'),
+  ],
+};
