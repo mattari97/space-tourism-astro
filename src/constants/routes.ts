faa9014d80ddfe64a6b02e6028018796ed043dec
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-08 20:30:57 -0400

    Extract APP_ROUTES in constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const APP_ROUTES: App.Route[] = [
+  { name: 'home', href: '/', base: '/' },
+  { name: 'destination', href: '/destination', base: '/destination' },
+  { name: 'crew', href: '/crew', base: '/crew' },
+  { name: 'technology', href: '/technology', base: '/technology' },
+];
