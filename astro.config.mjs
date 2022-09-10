2d887f8143d7f808aa936eaebee0ad01070f6ec5
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-10 05:57:36 -0400

    Remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import compress from "astro-compress";
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [compress()]
-});
+export default defineConfig({});
